bcae16e5089b4cd4b614acae01d510a82c3774e1
zychen <zeyu.chen07@gmail.com>
2026-06-22 16:19:17 +0800

    update

--- install_cu118.sh
+++ install_cu118.sh
@@ -1,5 +1,5 @@
 # install torch 2.3.0
-pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu118
+# pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu118
 pip install -U xformers==0.0.26.post1 --index-url https://download.pytorch.org/whl/cu118
 
 # install dependencies
@@ -11,8 +11,7 @@ pip install git+https://github.com/XPixelGroup/BasicSR
 
 cd ..
 # install pytorch3d
-pip install "git+https://github.com/facebookresearch/pytorch3d.git"
-
+pip install "git+https://github.com/facebookresearch/pytorch3d.git" --no-build-isolation
 # install sam2
 pip install git+https://github.com/hitsz-zuoqi/sam2/
 
@@ -21,14 +20,14 @@ pip install git+https://github.com/hitsz-zuoqi/sam2/
 # pip install ./sam2
 
 # install diff-gaussian-rasterization
-pip install git+https://github.com/ashawkey/diff-gaussian-rasterization/
+pip install git+https://github.com/ashawkey/diff-gaussian-rasterization/ --no-build-isolation
 
 # or
 # git clone --recursive https://github.com/ashawkey/diff-gaussian-rasterization
 # pip install ./diff-gaussian-rasterization
 
 # install simple-knn
-pip install git+https://github.com/camenduru/simple-knn/
+pip install git+https://github.com/camenduru/simple-knn/ --no-build-isolation
 
 # or
 # git clone https://github.com/camenduru/simple-knn.git
