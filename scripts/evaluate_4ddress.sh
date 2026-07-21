91abff40c21b0ed42dccf6ed1d36e17293991a71
zeyuc <zeyu.chen07@gmail.com>
2026-07-21 22:23:40 +0800

    update

--- scripts/evaluate_4ddress.sh
+++ scripts/evaluate_4ddress.sh
@@ -11,6 +11,6 @@ CUDA_VISIBLE_DEVICES=1 python -m LHM.launch train.human_lrm \
     --config configs/training/human-lrm-mini-4ddress.yaml \
     --sample-id 00127_Inner_Take10_00128 \
     --eval-only \
-    --mixed-precision no \
+    --mixed-precision bf16 \
     --checkpoint pretrained \
     "$@"
